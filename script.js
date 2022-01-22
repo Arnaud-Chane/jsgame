59460f0ac6eb2a11d95fb5e25c5df6f4f53a4011
Arnaud-Chane <59416180+Arnaud-Chane@users.noreply.github.com>
2022-01-22 14:28:58 +0100

    Add files via upload
    Calculating the distance between the right of the screen and the right of the ship

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Style Section
+// ------------------ Style Section ------------------ //
 
 // Style for the Ship
 var styleShip = document.createElement("style");
@@ -33,6 +33,14 @@ styleBtn.innerHTML = `
 document.head.appendChild(styleBtn);
 
 
+
+
+
+
+
+
+// ------------------ Buttons ------------------ //
+
 // Adding the Ship to body
 
 var imgPyu = document.createElement("img");
@@ -41,12 +49,33 @@ imgPyu.setAttribute("id", "pyuShip");
 document.body.appendChild(imgPyu);
 
 
+// Function to move the ship to the right
+const screenWidth = window.screen.width;
+console.log(screenWidth);
+//pixel from left to right base = ship
+var widthFromRightOfShip = 1500;
+var nbPxlMvRightShip = -5;
+
+finalWidthRight = widthFromRightOfShip + nbPxlMvRightShip;
+
+var stPxlMvRightShip = "px";
+var styleRightShip = finalWidthRight + stPxlMvRightShip;
+
+function moveToRightShip() {
+  var mRightShip = document.getElementById("pyuShip");
+//  nbPxlMvRightShip += -5;
+  mRightShip.style.right = styleRightShip;
+  console.log(styleRightShip);
+}
+
+
 // Adding right button
 
 var btnMoveRight = document.createElement("button");
 btnMoveRight.innerHTML = "RIGHT";
 btnMoveRight.setAttribute("class", "btnShipMove");
 btnMoveRight.setAttribute("id", "btnShipMoveRight");
+btnMoveRight.setAttribute("onclick", "moveToRightShip()");
 document.body.appendChild(btnMoveRight);
 
 
@@ -57,3 +86,4 @@ btnMoveLeft.innerHTML = "LEFT";
 btnMoveLeft.setAttribute("class", "btnShipMove");
 btnMoveLeft.setAttribute("id", "btnShipMoveLeft");
 document.body.appendChild(btnMoveLeft);
+

61653edfc59abb5aa6a8baadd09f65aa68d16871
Arnaud-Chane <59416180+Arnaud-Chane@users.noreply.github.com>
2022-01-24 21:34:23 +0100

    Add files via upload
    Adding button to spawn enemies and adding inline style

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var styleShip = document.createElement("style");
 styleShip.innerHTML = `
 #pyuShip {
   position: absolute;
+  top: 58%;
   height: 100px;
   width: 100px;
 }
@@ -15,19 +16,23 @@ document.head.appendChild(styleShip);
 
 var styleBtn = document.createElement("style");
 styleBtn.innerHTML = `
-.btnShipMove {
+.btn {
   position: absolute;
-  top: 250px;
-  height:20px;
+  top: 80%;
+  height:80px;
   width: 80px;
 }
 
 #btnShipMoveRight {
-  right: 25%;
+  right: 10%;
 }
 
 #btnShipMoveLeft {
-  left: 25%;
+  left: 10%;
+}
+
+#btnSpawnEnemies {
+  left: 37%;
 }
 `
 document.head.appendChild(styleBtn);
@@ -39,41 +44,46 @@ document.head.appendChild(styleBtn);
 
 
 
-// ------------------ Buttons ------------------ //
+// ------------------ Ship ------------------ //
 
 // Adding the Ship to body
 
 var imgPyu = document.createElement("img");
-imgPyu.src = "pyu.png";
+imgPyu.src = "pyuBig.png";
 imgPyu.setAttribute("id", "pyuShip");
 document.body.appendChild(imgPyu);
 
 
 // Function to move the ship to the right
-const screenWidth = window.screen.width;
-console.log(screenWidth);
+//const screenWidth = window.screen.width;
 //pixel from left to right base = ship
-var widthFromRightOfShip = 1500;
-var nbPxlMvRightShip = -5;
-
-finalWidthRight = widthFromRightOfShip + nbPxlMvRightShip;
-
-var stPxlMvRightShip = "px";
-var styleRightShip = finalWidthRight + stPxlMvRightShip;
+var positionPlayer = 0;
+const nbPxlMvRightShip = 25;
+const nbPxlMvLeftShip = -25;
+const strPxlMvShip = "px";
 
 function moveToRightShip() {
   var mRightShip = document.getElementById("pyuShip");
-//  nbPxlMvRightShip += -5;
-  mRightShip.style.right = styleRightShip;
-  console.log(styleRightShip);
+  positionPlayer += nbPxlMvRightShip;
+  var styleRightShip = positionPlayer + strPxlMvShip;
+  mRightShip.style.left = styleRightShip;
 }
 
+function moveToLeftShip() {
+  var mLeftShip = document.getElementById("pyuShip");
+  positionPlayer += nbPxlMvLeftShip;
+  var styleLeftShip = positionPlayer + strPxlMvShip;
+  mLeftShip.style.left = styleLeftShip;
+}
+
+
+// ------------------ Button ------------------ //
 
 // Adding right button
 
 var btnMoveRight = document.createElement("button");
 btnMoveRight.innerHTML = "RIGHT";
-btnMoveRight.setAttribute("class", "btnShipMove");
+btnMoveRight.setAttribute("class", "btn");
 btnMoveRight.setAttribute("id", "btnShipMoveRight");
 btnMoveRight.setAttribute("onclick", "moveToRightShip()");
 document.body.appendChild(btnMoveRight);
@@ -83,7 +93,37 @@ document.body.appendChild(btnMoveRight);
 
 var btnMoveLeft = document.createElement("button");
 btnMoveLeft.innerHTML = "LEFT";
-btnMoveLeft.setAttribute("class", "btnShipMove");
+btnMoveLeft.setAttribute("class", "btn");
 btnMoveLeft.setAttribute("id", "btnShipMoveLeft");
+btnMoveLeft.setAttribute("onclick", "moveToLeftShip()");
 document.body.appendChild(btnMoveLeft);
+
+
+
+// ------------------ Enemies ------------------ //
+
+var btnSpawnEnemies = document.createElement("button");
+btnSpawnEnemies.innerHTML = "Enemies Spawn";
+btnSpawnEnemies.setAttribute("class", "btn");
+btnSpawnEnemies.setAttribute("id", "btnSpawnEnemies");
+btnSpawnEnemies.setAttribute("onclick", "EnemySpawn()");
+document.body.appendChild(btnSpawnEnemies);
+
+
+function EnemySpawn(){
+  var enemies = document.createElement("div");
+  enemies.style = 
+  `
+    position: absolute; 
+    top: 20px; 
+    left: 70px; 
+    height: 50px; 
+    width: 50px; 
+    background-color: black; 
+    border-radius: 50%;
+  `;
+  
+  document.body.appendChild(enemies);
+  
+}
 
